79eba57e81c9d41d3f65f45fc60cbdb6c04a9547
Aycan <aycnkya12@gmail.com>
2022-08-02 15:36:46 +0300

    tag controller created

---
+++ WebApi/Controllers/TagController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Application.Interfaces;
+using System.Threading.Tasks;
+using Application.DTO.TagDTOs;
+
+namespace WebApi.Controllers
+{
+    public class TagController : ControllerBase
+    {
+        ITagService _tagService;
+        
+        public  TagController(ITagService tagService)
+        {
+            _tagService = tagService;
+        }
+        [HttpPost]
+        [Route("CreateTag")]
+        public async Task<IActionResult> CreateTag(CreateTagDTO createTagDTO)
+        {
+            return Ok(await _tagService.CreateTag(createTagDTO));
+
+        }
+        [HttpGet]
+        [Route("AllTags")]
+        public async Task<IActionResult> GelAllTags()
+        {
+            return Ok(await _tagService.GetAllTags());
+        }
+    }
+}

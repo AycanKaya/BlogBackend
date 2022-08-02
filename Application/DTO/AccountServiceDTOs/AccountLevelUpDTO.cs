065a872f952645eddb840dc5925873fbd5052282
Aycan <aycnkya12@gmail.com>
2022-08-02 15:33:42 +0300

    dto's updated and new dto's added

---
+++ Application/DTO/AccountServiceDTOs/AccountLevelUpDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTO
+{
+    public class AccountLevelUpDTO
+    {
+        public string UserID { get; set; }
+        public string LevelName { get; set; }
+
+    }
+}
---
+++ Application/DTO/PostTagDTOs/AddTagDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTO.PostTagDTOs
+{
+    public class AddTagDTO
+    {
+        public int TagID { get; set; }
+
+
+        public int PostID { get; set; }
+    }
+}

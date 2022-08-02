9e4bb01bcc309a28cb9c897a455ca00fe3a339a0
Aycan <aycnkya12@gmail.com>
2022-08-02 15:36:36 +0300

    created post tag controller

---
+++ WebApi/Controllers/PostTagController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Application.Interfaces;
+using Application.DTO.PostTagDTOs;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Authorize]
+    public class PostTagController : ControllerBase
+    {
+        IPostTagService _postTagService;
+
+        public PostTagController(IPostTagService service)
+        {
+            _postTagService = service;
+        }
+
+        [HttpPost]
+        [Route("AddTagToPost")]
+        public async Task<IActionResult> AddTagToPost(AddTagDTO addTagDTO)
+        {
+            return Ok(await _postTagService.AddTagToPost(addTagDTO));
+        }
+    }
+}

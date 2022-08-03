58a1f9be616cbf3c1e7de385c3bece39b8fecd24
Aycan <aycnkya12@gmail.com>
2022-08-03 17:27:30 +0300

    post response added

---
+++ Application/DTO/EditorUserDTOs/PostResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities;
+
+namespace Application.DTO.EditorUserDTOs
+{
+    public class PostResponse
+    {
+        public string AuthorName { get; set; }
+        public string AuthorEmail { get; set; }
+        public Post Post { get; set; }
+
+    }
+}

e11efd01473d6a803af086f64331f92ad3f81ebd
Aycan <aycnkya12@gmail.com>
2022-08-04 17:41:54 +0300

    dtos updated

---
+++ Application/DTO/PostServiceDTOs/PostResponseDTO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities;
+using Application.Interfaces;
+namespace Application.DTO
+{
+    public class PostResponseDTO
+    {
+
+        public string AuthorName { get; set; }
+        public string AuthorEmail { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public bool IsApprove { get; set; }
+        public DateTime CreateTime { get; set; }
+        public DateTime UpdateTime { get; set; }
+        public bool IsDeleted { get; set; }
+
+
+
+
+
+    }
+}

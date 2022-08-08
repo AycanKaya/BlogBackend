46f621f35f8499053c00e9f8eea462a942a4fefd
Aycan <aycnkya12@gmail.com>
2022-08-08 16:24:08 +0300

    Base response created

---
+++ Application/Wrappers/ResponseBase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Wrappers
+{
+    public class ResponseBase
+    {
+        public bool Succeeded { get; set; }
+        public string Message { get; set; }
+        public string Error { get; set; }
+        public int StatusCode { get; set; }
+        public void setResponseMessage(bool succed, string message, int code)
+        {
+            Succeeded = succed;
+            Message = message;
+            StatusCode = code;
+        }
+        public void SetErrorMessage(string error, bool succeed,string message, int statusCode)
+        {
+            Error = error;           
+            Message = message;
+            StatusCode = statusCode;
+            Succeeded = succeed;
+        }
+    }
+}

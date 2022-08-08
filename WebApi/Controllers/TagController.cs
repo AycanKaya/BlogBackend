600942b043bfa9b7de80d1cfdfa5e343ecc50b51
Aycan <aycnkya12@gmail.com>
2022-08-08 16:21:42 +0300

    Tag Controller edited responses that controller with response models

--- WebApi/Controllers/TagController.cs
+++ WebApi/Controllers/TagController.cs
@@ -3,29 +3,41 @@ using Microsoft.AspNetCore.Mvc;
 using Application.Interfaces;
 using System.Threading.Tasks;
 using Application.DTO.TagDTOs;
+using Application.Wrappers;
+using System.Net;
+using WebApi.Model;
 
 namespace WebApi.Controllers
 {
     public class TagController : ControllerBase
     {
         ITagService _tagService;
-        
-        public  TagController(ITagService tagService)
+
+        public TagController(ITagService tagService)
         {
             _tagService = tagService;
         }
         [HttpPost]
         [Route("CreateTag")]
-        public async Task<IActionResult> CreateTag(CreateTagDTO createTagDTO)
+        public async Task<ResponseBase> CreateTag(CreateTagDTO createTagDTO)
         {
-            return Ok(await _tagService.CreateTag(createTagDTO));
-
+            var tag = await _tagService.CreateTag(createTagDTO);
+            var response = new ResponseBase();
+            if (tag)
+            {
+                response.setResponseMessage(true, createTagDTO.TagName + " is created.", 200);
+                return response;
+            }
+            response.SetErrorMessage("Already Exists", false, "This tag already created!", (int)HttpStatusCode.Conflict);
+            return response;
         }
+
         [HttpGet]
         [Route("AllTags")]
-        public async Task<IActionResult> GelAllTags()
+        public async Task<TagsResponseModel> GelAllTags()
         {
-            return Ok(await _tagService.GetAllTags());
+            var tags = await _tagService.GetAllTags();
+            return new TagsResponseModel(tags, true, "All tags here", 200);
         }
     }
 }

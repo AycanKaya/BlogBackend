2edf04f95d148c586ca33788b51adb6539af251b
Aycan <aycnkya12@gmail.com>
2022-08-08 16:17:57 +0300

    DTOs for account

---
+++ Application/DTO/AccountServiceDTOs/AccountLevelResponseDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTO.AccountServiceDTOs
+{
+    public class AccountLevelResponseDTO
+    {
+        public int Level { get; set; }
+        public string LevelName { get; set; }
+        public int SumOfPosts { get; set; }
+    }
+}

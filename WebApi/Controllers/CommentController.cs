88f87a2bf55e24c063273899850fcd8733717673
Aycan <aycnkya12@gmail.com>
2022-08-08 16:20:46 +0300

    Comment Controller edited responses that controller with response models

--- WebApi/Controllers/CommentController.cs
+++ WebApi/Controllers/CommentController.cs
@@ -5,7 +5,8 @@ using Application.Interfaces;
 using Application.DTO;
 using Application.Wrappers;
 using Domain.Entities;
-using System.Collections.Generic;
+using System.Net;
+using WebApi.Model;
 
 namespace WebApi.Controllers
 {
@@ -23,24 +24,56 @@ namespace WebApi.Controllers
             _authenticatedUserService = authenticatedUserService;
         }
         [HttpDelete("DeleteComment")]
-        public async Task<BaseResponse<string>> DeleteComment(int commentID)
+        public async Task<ResponseBase> DeleteComment(int commentID)
         {
             var token = HttpContext.Request.Headers.Authorization.ToString();
-            var deleteComment = await _commentService.DeleteComment(commentID, token);
-            return deleteComment;
+            var isDeleted = await _commentService.DeleteComment(commentID, token);
+            var response = new ResponseBase();
+            if (isDeleted)
+            {
+                response.setResponseMessage(true, "Comment deleted successfully.", (int)HttpStatusCode.OK);
+                return response;
+            }
+            response.SetErrorMessage("Bad Request", false, "Only users who share the post or comment can delete the comment !", (int)HttpStatusCode.BadRequest);
+            return response;
+
+
         }
+
+
         [HttpPost("ShareComment")]
-        public async Task<BaseResponse<Comment>> ShareComment(CommentDTO commentDTO)
+        public async Task<ResponseBase> ShareComment(CommentDTO commentDTO)
         {
             var token = HttpContext.Request.Headers.Authorization.ToString();
-            return new BaseResponse<Comment>(await _commentService.ShareComment(token, commentDTO));
+            var comment = await _commentService.ShareComment(token, commentDTO);
+            return new ResponseBase()
+            {
+                Succeeded = true,
+                Message = "Comment shared successfully.",
+                StatusCode = (int)HttpStatusCode.OK,
+            };
+
         }
-        
-     
+
+
         [HttpGet("GetComments")]
-        public async Task<BaseResponse<List<Comment>>> GetAllComments(int postId)
+        public async Task<GetCommentsResponeModel> GetAllComments(int postId)
         {
-            return new BaseResponse<List<Comment>>(await _commentService.GetComments(postId));
+            var comments = await _commentService.GetComments(postId);
+            return new GetCommentsResponeModel(comments, true, "Comments belong to post", 200);
+
+        }
+        [HttpPut("UpdateComment")]
+        public async Task<ResponseBase> UpdateComment(UpdateCommentDTO updateComment)
+        {
+            var isUpdate = await _commentService.UpdateComment(updateComment);
+            return new ResponseBase()
+            {
+                Succeeded = isUpdate,
+                Message = "Comment updated successfully.",
+                StatusCode = (int)HttpStatusCode.OK,
+            };
+
         }
     }
 }

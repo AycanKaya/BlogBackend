5e3e30f2ca9038922fe3749f6224c2b295dbbac9
Aycan <aycnkya12@gmail.com>
2022-08-08 16:21:16 +0300

    EditorUserController Controller edited responses that controller with response models

--- WebApi/Controllers/EditorUserController.cs
+++ WebApi/Controllers/EditorUserController.cs
@@ -4,6 +4,9 @@ using Application.Interfaces;
 using System.Threading.Tasks;
 using Application.DTO;
 using Application.DTO.EditorUserDTOs;
+using Application.Model;
+using Application.Wrappers;
+using System.Net;
 
 namespace WebApi.Controllers
 {
@@ -11,7 +14,7 @@ namespace WebApi.Controllers
     [ApiController]
 
     [Authorize(Roles = "Editor")]
-    public class EditorUserController :ControllerBase
+    public class EditorUserController : ControllerBase
     {
         IEditorUserService _editorUserService;
         public EditorUserController(IEditorUserService editorUserService)
@@ -21,24 +24,44 @@ namespace WebApi.Controllers
 
         [HttpGet]
         [Route("GetPassivePosts")]
-        public async Task<IActionResult> GetPassivePosts()
+        public async Task<PostsResponseModel> GetPassivePosts()
         {
-            return Ok(await _editorUserService.GetPassivePosts());
+            var posts = await _editorUserService.GetPassivePosts();
+            var response = new PostsResponseModel(posts, true, "All passive posts", 200);
+            return response;
         }
-       
+
 
         [HttpPost]
         [Route("ActivateControl")]
-        public async Task<IActionResult> ActivatePost(ApproveControlDTO approveControl)
+        public async Task<ResponseBase> ActivatePost(ApproveControlDTO approveControl)
         {
-            return Ok(await _editorUserService.ActivatePost(approveControl));
+            var changeActivate = await _editorUserService.ActivatePost(approveControl);
+            var response = new ResponseBase();
+            if (changeActivate)
+            {
+                response.setResponseMessage(true, "Post approve changed", 200);
+                return response;
+            }
+            response.SetErrorMessage("Not change", false, "Fault", (int)HttpStatusCode.InternalServerError);
+            return response;
+
         }
 
-        [HttpDelete]
-        [Route("DeletePost")]
-        public async Task<IActionResult> DeletePost(int commentID)
+        [HttpPost]
+        [Route("DeleteComment")]
+        public async Task<ResponseBase> DeleteComment(int commentID)
         {
-            return Ok(await _editorUserService.DeleteComment(commentID));
+            var isDelete = await _editorUserService.DeleteComment(commentID);
+            var response = new ResponseBase();
+            if (isDelete)
+            {
+                response.setResponseMessage(true, "Comment deleted!", 200);
+                return response;
+
+            }
+            response.SetErrorMessage("Comment not found!", false, "Comment did not delete!", (int)HttpStatusCode.BadRequest);
+            return response;
         }
 
     }

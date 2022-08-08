2c9253f3f71da453aaee6d7af3645112494f7c1d
Aycan <aycnkya12@gmail.com>
2022-08-08 16:19:09 +0300

    ResponseModel for post

---
+++ Application/Model/PostResponseModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.DTO;
+using Application.Wrappers;
+
+namespace Application.Model
+{
+    public class PostResponseModel : ResponseBase
+    {   
+        public PostResponseDTO? Post { get; set; }
+        public PostResponseModel(PostResponseDTO post,bool succeed, string message, int code)
+        {
+            Post = post;
+            Succeeded = succeed;
+            Message = message;
+            StatusCode = code;
+        }
+
+    }
+}
---
+++ Application/Model/PostsResponseModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.DTO;
+using Application.Wrappers;
+
+namespace Application.Model
+{
+    public class PostsResponseModel :ResponseBase
+    {
+        public PostResponseDTO[]? Posts { get; set; }
+        public PostsResponseModel(PostResponseDTO[] posts, bool succeed, string message,int code)
+        {
+            Posts = posts;
+            Succeeded = succeed;
+            Message = message;
+            StatusCode = code;
+        }
+    }
+}

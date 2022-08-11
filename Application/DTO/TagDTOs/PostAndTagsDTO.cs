1a8210ed890842c0ba5cfe0d1892402ad7d0a5e8
Aycan <aycnkya12@gmail.com>
2022-08-11 15:40:19 +0300

    DTOs added

---
+++ Application/DTO/TagDTOs/PostAndTagsDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.DTO.PostServiceDTOs;
+using Domain.Entities;
+
+namespace Application.DTO.TagDTOs
+{
+    public class PostAndTagsDTO
+    {
+        public PostCommentsDTO Post { get; set; }
+        public Tag[] Tags { get; set; }
+    }
+}
---
+++ Application/DTO/TagDTOs/PostInTagsDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTO.TagDTOs
+{
+    public class PostInTagsDTO
+    {
+        public int PostID { get; set; }
+        public string TagName { get; set; }
+    }
+}

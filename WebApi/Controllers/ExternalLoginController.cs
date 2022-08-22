bea743c93af168119044d5d77afa75cd090dbb43
Aycan <aycnkya12@gmail.com>
2022-08-22 17:49:18 +0300

    External login controllers for auth0 with httpClient

--- WebApi/Controllers/ExternalLoginController.cs
+++ WebApi/Controllers/ExternalLoginController.cs
@@ -1,31 +1,71 @@
 ﻿using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
-using Application.Wrappers;
 using Microsoft.AspNetCore.Mvc;
 using RestSharp;
+using System;
+using WebApi.Model;
+using System.Text.Json;
+using System.Collections.Generic;
 
 namespace WebApi.Controllers
 {
-    /*
-     *   request.AddHeader("content-type", "application/x-www-form-urlencoded");
-            request.AddParameter("application/x-www-form-urlencoded", "grant_type=client_credentials&client_id=GfymX8bRn4g2epD7CxmCU8yzUY35gryv&client_secret=pI1VLYcaAZGKPwNbduUZ_-Ij092REOu7cos3bf9CFwPKfJ87R8bYFlmzcNsrqrE5&audience=https://myapi-server.com", ParameterType.RequestBody);
-     */
+   
 
     [Route("api/[controller]")]
     [ApiController]
     public class ExternalLoginController :ControllerBase
     {
-       
-
-        [HttpGet("AccessToken")]
-        public async Task<IActionResult> getAccessToken()
+        private readonly HttpClient _httpClient;
+        public ExternalLoginController()
         {
-            var client = new RestClient("https://dev-y-mn-g3v.us.auth0.com/oauth/token");
-            var request = new RestRequest(Method.Post.ToString());
-            request.AddHeader("content-type", "application/json");
-            request.AddParameter("application/json", "{\"client_id\":\"kl2tZvpqSUBr5rzeWuT5BxGzq8nvhIeI\",\"client_secret\":\"OdsqIZiB70AJXHiAOh0ASd78wNfxEj1xLNr1VUs8Qw2lR9CM8KUqqyZdKBHJBWOT\",\"audience\":\"https://dev-y-mn-g3v.us.auth0.com/api/v2/\",\"grant_type\":\"client_credentials\"}", ParameterType.RequestBody);
-            return Ok(client.Execute(request));
+            _httpClient=new HttpClient();
+
+         
+            _httpClient.BaseAddress = new Uri("https://dev-y-mn-g3v.us.auth0.com");
+            _httpClient.DefaultRequestHeaders.Clear();
+            _httpClient.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
         }
+
+        private async Task<T> Get<T>(string required) where T : ExternalLoginModel
+        {
+
+
+            var response = await _httpClient.GetAsync(required);
+            response.EnsureSuccessStatusCode();
+            var content = await response.Content.ReadAsStringAsync();
+            return JsonSerializer.Deserialize<T>(content);
+        }
+
+
+        [HttpPost("external-login")]
+        public async Task<IActionResult> LoginWithAuth0()
+        {
+
+
+            var content = new FormUrlEncodedContent(new[]
+            {
+                new KeyValuePair<string, string>("client_id", "GHex6lI47lr8Dje8xtQAK5szb32eUY8E"),
+                new KeyValuePair<string, string>("client_secret", "DlgdfOPMMRgFD2LLY9YS_jF-J52pEwKlRqM8hNy-r0pnIPYTpIP_9O8CQ508v_Xh"),
+                new KeyValuePair<string, string>("audience", "https://blogserver.com"),
+                new KeyValuePair<string, string>("grant_type", "client_credentials")
+            }) ; 
+            var result = await _httpClient.PostAsync("/oauth/token", content);
+            string resultContent = await result.Content.ReadAsStringAsync();
+            Console.WriteLine(resultContent);
+
+
+            var client = new RestClient("https://dev-y-mn-g3v.us.auth0.com/oauth/token");
+            var request = new RestRequest("POST");
+            request.AddHeader("content-type", "application/json");
+            request.AddParameter("application/json", "{\"client_id\":\"GHex6lI47lr8Dje8xtQAK5szb32eUY8E\",\"client_secret\":\"DlgdfOPMMRgFD2LLY9YS_jF-J52pEwKlRqM8hNy-r0pnIPYTpIP_9O8CQ508v_Xh\",\"audience\":\"https://blogserver.com\",\"grant_type\":\"client_credentials\"}", ParameterType.RequestBody);
+           
+            return Ok(resultContent);
+        }
+
+
+    
+
     }
 }
